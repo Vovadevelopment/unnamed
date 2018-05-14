a663065110fcd8670ae12be8c2c2da4424cb70fd
AnnaAkibarova <Akibarova@gmail.com>
2018-05-14 12:07:27 +0300

    style & content changes

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -8,7 +8,6 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class ServicesComponent implements OnInit {
   constructor() {
