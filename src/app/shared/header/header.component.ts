9d2de8f1e93868a48654cdde85d2f39214de5215
AnnaAkibarova <Akibarova@gmail.com>
2018-06-10 23:18:51 +0300

    mobile header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,18 +1,22 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {EventsService} from '../../services/events.service';
 import {Subject} from 'rxjs/Subject';
 import {NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 import {MessageService} from 'primeng/components/common/messageservice';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
   @ViewChild('headerMark') headerMark: ElementRef;
   headerFixed: boolean = false;
-  isMobileMenuActive: boolean = false;
+  isMobileMenuActive:boolean = false;
+  public innerWidth: any;
+  showMobileMenu:boolean = false;
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
     // console.log('Scrolling');
@@ -20,11 +24,11 @@ export class HeaderComponent implements OnInit {
   }
   @HostListener('window:resize', ['$event']) onResize(event) {
     this.isMobileMenuActive = event.target.innerWidth < 890;
-    console.log(event.target.innerWidth);
+    // this.isMobileMenuActive = window.innerWidth < 890;
+    console.log(window.innerWidth);
   }
 
   constructor(private eventsService: EventsService, private router: Router, private messagesService: MessageService) {
-
     router.events.subscribe( (event) => {
 
       if (event instanceof NavigationStart) {
@@ -43,12 +47,14 @@ export class HeaderComponent implements OnInit {
       }
     });
   }
-
+  mobileButton() {
+    return this.showMobileMenu = true;
+  }
   ngOnInit() {
+    this.isMobileMenuActive = window.innerWidth < 890;
     this.eventsService.scrollSubject.subscribe((event) => {
       console.log(this.eventsService.isElementInViewport(this.headerMark.nativeElement));
-      this.headerFixed = !this.eventsService.isElementInViewport(this.headerMark.nativeElement)
-    })
+      this.headerFixed = !this.eventsService.isElementInViewport(this.headerMark.nativeElement);
+    });
   }
-
 }
