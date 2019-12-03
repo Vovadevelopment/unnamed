6be16ed30f0c836ca54ad8ba3fa74f2c01ec426c
Kredynets Volodymyr <vova@deepinspire.com>
2019-12-03 21:09:30 +0200

    added description to services (first version)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit {
+  title = 'Клінінгова компанія Clinko | Прибирання Львів | Хімчистка меблів львів';
+  constructor(private titleService: Title, private meta: Meta) {}
 
-  constructor() {}
-
-  ngAfterViewInit() {
-    // alert(document.getElementById("foo").offsetTop)
+  ngOnInit()  {
+    this.meta.updateTag({
+      name: 'keywords', content: 'генеральне прибирання Львів, прибирання після ремонту львів, миття вікон львів, прибирання в неробочий та нічний час, хімчистка меблів львів ',
+    });
+    this.meta.updateTag({
+      name: 'description', content: 'Послуги:-генеральне прибирання Львів; -хімчистка меблів; -прибирання в неробочий та нічний час львів; -прибирання після ремонту львів,-миття вікон львів; '
+    });
+    this.titleService.setTitle(this.title);
   }
 
 
